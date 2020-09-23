e17561cb54c8ebc08346e7b1703027ae504003be
kurucc <kdbmgt@gmail.com>
2020-09-23 19:37:03 +0200

    Fixed some bugs in python script and yml file

--- units/run_battle_test.py
+++ units/run_battle_test.py
@@ -9,7 +9,6 @@ for filename1 in os.listdir(directory):
     for filename2 in os.listdir(directory):
         if(filename1 != filename2):
             if filename2.endswith(".json"):
-                os.system("../../build-mosze_01-Desktop_Qt_5_14_1_GCC_64bit-Debug/mosze_01 " + filename1 + " " + filename2)
-                #print(filename1 + " " +filename2)
+                os.system("mosze_01 " + filename1 + " " + filename2)
         else:
             continue
