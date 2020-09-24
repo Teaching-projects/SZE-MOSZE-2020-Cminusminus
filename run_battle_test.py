acdae60789746d30a4959946e07ccb2419806f66
bricsi0000000000000 <bolya.richard@gmail.com>
2020-09-24 07:24:30 +0200

    Fixed github actions file paths and moved test script to root folder

--- run_battle_test.py
+++ run_battle_test.py
@@ -2,13 +2,13 @@
 
 import os
 
-directory = "."
+directory = "units/"
 for filename1 in os.listdir(directory):
     if filename1.endswith(".json") == False:
         continue
     for filename2 in os.listdir(directory):
         if(filename1 != filename2):
             if filename2.endswith(".json"):
-                os.system("mosze_01" + filename1 + " " + filename2)
+                os.system("./mosze_01 " + directory + filename1 + " " + directory + filename2)
         else:
             continue
