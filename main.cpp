99c14742dea7908162ad275b7c65c395c66d33db
kurucc <kdbmgt@gmail.com>
2020-09-26 21:10:38 +0200

    Fixed file existance checking and removed additional brackets

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ std::vector<std::string> splittedString(std::string text, char delimiter){
 
   for(unsigned int i = 0; i < text.length(); i++){
     if(text[i] == delimiter){
-      splittedStrings.push_back((addString));
+      splittedStrings.push_back(addString);
       addString = "";
     }
     else{
@@ -35,16 +35,11 @@ std::vector<std::string> splittedString(std::string text, char delimiter){
     }
   }
 
-  splittedStrings.push_back((addString));
+  splittedStrings.push_back(addString);
 
   return splittedStrings;
 }
 
-bool fileExists(const std::string& fileName)
-{
-	std::ifstream f(fileName.c_str());
-	return f.good();
-}
 
 static Character* parseUnit(const std::string& fileName){
   std::string name = "";
@@ -76,6 +71,10 @@ static Character* parseUnit(const std::string& fileName){
     }
     characterDataFile.close();
   }
+  else 
+  {
+	  throw 1;
+  }
 
   return new Character(name, health, damage);
 }
@@ -87,41 +86,17 @@ int main(int argc, char *argv[])
 
     return 1;
   }
-  else{
-	  unsigned int counter = 0;
-	  for (int i = 1; i < argc; i++)
-	  {
-		  try
-		  {
-
-			  bool exists = fileExists(argv[i]);
-			  throw exists;
-		  }
-		  catch (const bool& exists)
-		  {
-			  if (!exists&&i==1)
-			  {
-				  std::cout << "First file doesn't exists!\n";
-				  counter++;
-			  }
-			  else if (!exists&&i == 2)
-			  {
-				  std::cout << "Second file doesn't exists!\n";
-				  counter++;
-			  }
-		  }
-	  }
-	  if (counter!=0)
-	  {
-		  return 1;
-	  }
-	  else
+  else
+  {
+	  try
 	  {
 		  battle(*parseUnit(argv[1]), *parseUnit(argv[2]));
 	  }
-
-	  
-    
+	  catch (const int& ex)
+	  {
+		  std::cout << "First and/or second file doesn't exists.\n";
+		  exit(ex);
+	  }	 
   }
 
   return 0;
