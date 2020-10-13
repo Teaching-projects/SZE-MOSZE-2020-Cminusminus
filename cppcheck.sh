96a36b86b4152c4f8535f19e5238c0c0809ec2ea
kurucc <kdbmgt@gmail.com>
2020-10-13 19:57:13 +0200

    Final cppcheck.sh fix

--- cppcheck.sh
+++ cppcheck.sh
@@ -3,16 +3,17 @@
 IFS=$'\n'
 
 
-$(cppcheck *.cpp --enable=warning --output-file=cppcheck_errors.txt)
+echo "$(cppcheck *.cpp --enable=warning --output-file=cppcheck_errors.txt)"
 	if  [ ! -s .cppcheck_errors.txt ]
 	then
 			echo "Not found any errors and warnings."
-			 $(cppcheck *.cpp --enable=performance,style --output-file=cppcheck_performance.txt)
+			 echo "$(cppcheck *.cpp --enable=performance,style --output-file=cppcheck_performance.txt)"
 		
 		if [ ! -s cppcheck_performance.txt ]
 		then
 			echo "Not found any performance and style errors."
 		fi
 	else
+			echo "Found some errors or warnings."
 			exit 1
 	fi 
