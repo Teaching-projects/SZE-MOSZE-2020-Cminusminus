81c041ba8aae39862b868dc39ead408227ef0635
kurucc <kdbmgt@gmail.com>
2020-10-13 19:42:13 +0200

    Update cppcheck.sh

--- cppcheck.sh
+++ cppcheck.sh
@@ -4,9 +4,9 @@ IFS=$'\n'
 
 
 echo "$(cppcheck *.cpp --enable=warning --output-file=cppcheck_errors.txt)"
-	if ![-s .cppcheck_errors.txt]
+	if  [ ! -s .cppcheck_errors.txt ]
 	then
 		exit 1
 	else
 		echo "$(cppcheck *.cpp --enable=performance, style --output-file=cppcheck_performance.txt)"
-	fi 
+	fi
