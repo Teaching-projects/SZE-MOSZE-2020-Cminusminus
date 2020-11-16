1775f7545f2b1bc86060a3555115f694f96de676
kurucc <kdbmgt@gmail.com>
2020-11-16 17:42:57 +0100

    Update characterMaker.h

--- characterMaker.h
+++ characterMaker.h
@@ -12,7 +12,7 @@
 class CharacterMaker{
 public:
     Character* createCharacter(std::map<std::string, std::string> parsedData);
-    Player* createPlayer(std::map<std::string, std::string> parsedData);
+    //Player* createPlayer(std::map<std::string, std::string> parsedData);
 private:
     void showErrorMessage(std::string message);
 };
