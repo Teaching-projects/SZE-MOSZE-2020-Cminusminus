c93886116f792b3ba7a2616a3c34d25211883989
fruzsin <mfru99@gmail.com>
2020-11-21 12:29:32 +0100

    Update main_test.cpp

--- main_test.cpp
+++ main_test.cpp
@@ -16,37 +16,6 @@ TEST(ParseFromStringTest, checkIfEquals){
   EXPECT_FLOAT_EQ(json.get<float>("atc"), 1.4);
 }
 
-TEST(ParsingTest, checkIfEquals){
-	
-	JSON json = JSON::parseFromFile("units/test_unit_1.json");
-
-  EXPECT_EQ(json.get<std::string>("name", "Béla")
-  EXPECT_EQ(json.get<int>("hp", 1500000)
-  EXPECT_EQ(json.get<int>("dmg", 14)
-  EXPECT_FLOAT_EQ(json.get<float>("attackCooldown"), 1.4);
-}
-
-TEST(BadFileTest, checkIfGood){
-	
-	std::string file = "bad_units/test_unit_badDMG.json";
-
-    try{
-        JSON::parseFromFile(testfile);
-        ASSERT_TRUE(true);
-    } catch(std::runtime_error& e){
-        ASSERT_STREQ(e.what(), "Bad JSON file.");
-    }
-}
-
-TEST(LVLupTest, checkHero){
-	
-	Hero testhero ("Hero", 500, 10, 50, 20, 20, 0.9, 1.1);
-	Monster testmonster("Monster", 500, 20, 2.5);
-	testhero.fightTilDeath(testmonster);
-	EXPECT_EQ(5,testhero.getLevel());
-
-}
-
 int main(int argc, char **argv){
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
