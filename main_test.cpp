862d6ff3f42a25b350df2db71b88d24ae060c1a8
kurucc <kdbmgt@gmail.com>
2020-11-27 11:12:24 +0100

    Update main_test.cpp

--- main_test.cpp
+++ main_test.cpp
@@ -254,16 +254,11 @@ TEST(checkWrongMapName, checkGame){
     }
 
 }
+
 TEST(checkWrongIndex, checkMap){
 	
-	
-	try{
-	Map map("map2.txt");
-	map.get(20,30);
-	ASSERT_EQ(1,map.get(0,5));
-    } catch(std::exception& e){
-        ASSERT_STREQ(e.what(), "WrongIndexException!");
-    }
+	Map mapp("map2.txt");
+	ASSERT_EQ(1,mapp.get(0,5))
 
 }
 
