b80606a6a9239881629e0501b05fa933e67c4f3e
kurucc <kdbmgt@gmail.com>
2020-11-29 14:55:07 +0100

    added doxygen documentation

--- MarkedMap.h
+++ MarkedMap.h
@@ -6,10 +6,25 @@
 
 class MarkedMap : public Map
 {
+
+/**
+*\brief A class for loading a marked map.
+*\param filename The filename we load.
+*/
+
 public:
 	MarkedMap(std::string filename);
+	///\brief Function to get the free spaces or the walls on the map.
+	///\param x The rows of the map
+	///\param y The columns of the map
+	///\return Map::type
 	Map::type get(int x, int y) const override;
+	///\brief Function to get the hero's position.
+	///\return std::pair<int,int>
 	std::pair<int,int> getHeroPosition() const;
+	///\brief Function to get the hero's position.
+	///\param c The monster's type we want to get
+	///\return std::list<std::pair<int,int>> Function to get a monster's position.
 	std::list<std::pair<int,int>> getMonsterPosition(char c) const;
 private:
 	std::vector<std::string> markedmap;
